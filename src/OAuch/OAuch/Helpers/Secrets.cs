f71ba5777606930b9d053abee08ebe04b7530769
Pieter Philippaerts <pp@pieterp.be>
2024-11-28 12:22:03 +0100

    Merge to latest version and updated packages to .NET 9

--- src/OAuch/OAuch/Helpers/Secrets.cs
+++ src/OAuch/OAuch/Helpers/Secrets.cs
@@ -5,21 +5,16 @@ namespace OAuch.Helpers {
         public string? CaptchaSiteKey { get; set; }
         public  string? CaptchaSiteSecret { get; set; }
         
-        public  string EMailPassword { get; set; }
-        public string EMailFrom { get; set; }
-        public string EMailTo { get; set; }
-        public string EMailHost { get; set; }
-
-        //public string FacebookClientId { get; set; }
-        //public  string FacebookClientSecret { get; set; }
-        
-        public  string? GoogleClientId { get; set; }
-        public  string? GoogleClientSecret { get; set; }
+        public  string? EMailPassword { get; set; }
+        public string? EMailFrom { get; set; }
+        public string? EMailTo { get; set; }
+        public string? EMailHost { get; set; }
+        public string? MicrosoftClientId { get; set; }
+        public string? MicrosoftClientSecret { get; set; }
+        public string? GoogleClientId { get; set; }
+        public string? GoogleClientSecret { get; set; }
 
         [MemberNotNullWhen(true, nameof (EMailHost), nameof(EMailTo), nameof(EMailFrom), nameof(EMailPassword))]
         public bool HasMailSettings() => EMailHost != null && EMailTo != null && EMailFrom != null && EMailPassword != null;
-
-        //public  string TwitterClientId { get; set; }
-        //public  string TwitterClientSecret { get; set; }
     }
 }
--- src/OAuch/OAuch/Startup.cs
+++ src/OAuch/OAuch/Startup.cs
@@ -55,39 +55,23 @@ namespace OAuch {
                     options.LoginPath = "/Home";
                     options.ClaimsIssuer = "OAUCH";
                 });
-            if (secrets.GoogleClientId != null)
+
+            if (secrets.GoogleClientId != null) {
                 servBuilder = servBuilder.AddGoogle(options => {
                     options.ClientId = secrets.GoogleClientId;
                     options.ClientSecret = secrets.GoogleClientSecret ?? "";
                     options.UsePkce = true;
                     options.AccessDeniedPath = "/";
-                })
-                //.AddFacebook(options => {
-                //    options.ClientId = secrets.FacebookClientId;
-                //    options.ClientSecret = secrets.FacebookClientSecret;
-                //    options.UsePkce = true;
-                //    options.AccessDeniedPath = "/";
-                //})
-                .AddMicrosoftAccount(options => {
-                    options.ClientId = secrets.MicrosoftClientId;
-                    options.ClientSecret = secrets.MicrosoftClientSecret ?? "";
-                    options.UsePkce = true;
-                    options.AccessDeniedPath = "/";
                 });
-
-            //if (secrets.FacebookClientId != null)
-            //    servBuilder = servBuilder.AddFacebook(options => {
-            //        options.ClientId = secrets.FacebookClientId;
-            //        options.ClientSecret = secrets.FacebookClientSecret ?? "";
-            //        options.UsePkce = true;
-            //        options.AccessDeniedPath = "/";
-            //    });
-            //.AddTwitter(options => {
-            //    options.ConsumerKey = secrets.TwitterClientId;
-            //    options.ConsumerSecret = secrets.TwitterClientSecret;
-            //    options.RetrieveUserDetails = false;
-            //    options.AccessDeniedPath = "/";
-            //});
+            }
+            if (secrets.MicrosoftClientId != null) {
+                servBuilder = servBuilder.AddMicrosoftAccount(options => {
+                     options.ClientId = secrets.MicrosoftClientId;
+                     options.ClientSecret = secrets.MicrosoftClientSecret ?? "";
+                     options.UsePkce = true;
+                     options.AccessDeniedPath = "/";
+                 });
+            }
 
             // add logging services
             services.AddSingleton<ILogConverter<Exception>>(new ExceptionConverter());
